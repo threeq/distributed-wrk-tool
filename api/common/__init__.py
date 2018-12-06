ca8758ed7603cd9a2463e124fb553b9d8f3a84d0
three <448987046@qq.com>
2018-12-06 22:12:03 +0800

    mv api and ui to flask-seed

--- api/common/__init__.py
+++
@@ -1,6 +0,0 @@
-def assert_ok_code(code):
-    assert 400 > code >= 200
-
-
-def assert_fail_code(code):
-    assert 200 > code or code >= 400
--- api/common/exception.py
+++
@@ -1,8 +0,0 @@
-from api.common.code_def import Code
-
-
-class DomainException(Exception):
-    def __init__(self, code=Code.ERROR, msg=None):
-        self.code = code
-        self.msg = msg
-
